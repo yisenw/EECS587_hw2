47ae3383dd44063996688f31f972334652804f0a
yisenw <yisenw@umich.edu>
2021-09-27 18:34:23 -0400

    587sb

--- serial_program.cpp
+++ serial_program.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
 // Initialize the MPI environment.
     MPI_Init(&argc, &argv);
     // Obtain my id and the world size.
-    MPI_BXOR(MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
     double start = MPI_Wtime();
 
     int n = 4;
