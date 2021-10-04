83f43a6fe5f19669a03a55820734730e1d0b3537
yisenw <yisenw@umich.edu>
2021-10-04 18:18:58 -0400

    try calculate!

--- parallel_program.cpp
+++ parallel_program.cpp
@@ -65,118 +65,120 @@ void run_parallel(int n, long long (*f)(long long, long long, long long, long lo
 
     // start sending 
     // right send to left; lower send to upper; lower right send to upper left.
-    if (col != 0) { // right send to left
-        vector<long long> msg;
-        get_first_col(msg, A0);
-        // if (if_print) {
-        //     cout << "Sending msg from right " << ID << " of " << P << " to " << ID - 1 << ": ";
-        //     for (long long elt: msg) cout << elt << " ";
-        //     cout << endl;
-        // }
-        // long long* a = &msg[0];
-        MPI_Send(&msg[0], msg.size(), MPI_LONG_LONG, ID - 1, 0, MPI_COMM_WORLD);
-    }
-    if (row != 0) { // lower send to upper
-        vector<long long> msg = A0[0];
-        // if (if_print) {
-        //     cout << "Sending msg from lower " << ID << " of " << P << " to " << ID - n_of_P << ": ";
-        //     for (long long elt: msg) cout << elt << " ";
-        //     cout << endl;
-        // }
-        MPI_Send(&msg[0], msg.size(), MPI_LONG_LONG, ID - n_of_P, 0, MPI_COMM_WORLD);
-    }
-    if (col != 0 && row != 0) { // lower l_r to u_l
-        long long msg = A0[0][0];
-        // if (if_print) {
-        //     cout << "Sending msg from l_r " << ID << " of " << P << " to " << ID - 1 - n_of_P << ": ";
-        //     cout << msg;
-        //     cout << endl;
-        // }
-        MPI_Send(&msg, 1, MPI_LONG_LONG, ID - 1 - n_of_P, 0, MPI_COMM_WORLD);
-    }
+    for (int it = 0; it < IT_NUM; it++) {
 
-    long long last_row[num_col];
-    long long last_col[num_row];
-    long long l_r = -1;
-    // start receiving
-    if (col != n_of_P - 1) { // not the rightest, receive from right
-        MPI_Recv(&last_col, num_row, MPI_LONG_LONG, ID + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    if (row != n_of_P - 1) { // not the lowest
-        MPI_Recv(&last_row, num_col, MPI_LONG_LONG, ID + n_of_P, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    if (row != n_of_P - 1 && col != n_of_P - 1) { // not the lowest or rightest
-        MPI_Recv(&l_r, 1, MPI_LONG_LONG, ID + n_of_P + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-
-    if (if_print) {
-        if (col != n_of_P - 1) {
-            cout << "Last col of " << ID << ", ";
-            for (long long elt: last_col) cout << elt << " ";
+        if (col != 0) { // right send to left
+            vector<long long> msg;
+            get_first_col(msg, A0);
+            // if (if_print) {
+            //     cout << "Sending msg from right " << ID << " of " << P << " to " << ID - 1 << ": ";
+            //     for (long long elt: msg) cout << elt << " ";
+            //     cout << endl;
+            // }
+            // long long* a = &msg[0];
+            MPI_Send(&msg[0], msg.size(), MPI_LONG_LONG, ID - 1, 0, MPI_COMM_WORLD);
         }
-        if (row != n_of_P - 1) {
-            cout << "Last row of " << ID << ", ";
-            for (long long  elt: last_row) cout << elt << " ";
+        if (row != 0) { // lower send to upper
+            vector<long long> msg = A0[0];
+            // if (if_print) {
+            //     cout << "Sending msg from lower " << ID << " of " << P << " to " << ID - n_of_P << ": ";
+            //     for (long long elt: msg) cout << elt << " ";
+            //     cout << endl;
+            // }
+            MPI_Send(&msg[0], msg.size(), MPI_LONG_LONG, ID - n_of_P, 0, MPI_COMM_WORLD);
         }
-        cout << "LR of " << ID << ", ";
-        cout << l_r << "\n";
-    }
-
-    // begin calculation.
-    vector<vector<long long>> A(num_row, vector<long long> (num_col, 0));
-    int start_row = 0;
-    int start_col = 0;
-    if (row == 0) {
-        start_row = 1;
-        for (int j = 0; j < num_col; j++) A[0][j] = A0[0][j];
-    }
-    if (col == 0) {
-        start_col = 1;
-        for (int i = 0; i < num_row; i++) A[i][0] = A0[i][0];
-    } // upperest and leftest are handled
-
-    for (int i = start_row; i < num_row - 1; i++) { // handle mid elements
-        for (int j = start_col; j < num_col - 1; j++) {
-            A[i][j] = f(A0[i][j], A0[i + 1][j], A0[i][j + 1], A0[i + 1][j + 1]);
+        if (col != 0 && row != 0) { // lower l_r to u_l
+            long long msg = A0[0][0];
+            // if (if_print) {
+            //     cout << "Sending msg from l_r " << ID << " of " << P << " to " << ID - 1 - n_of_P << ": ";
+            //     cout << msg;
+            //     cout << endl;
+            // }
+            MPI_Send(&msg, 1, MPI_LONG_LONG, ID - 1 - n_of_P, 0, MPI_COMM_WORLD);
         }
-    }
-    if (col != n_of_P - 1) { // not the rightest
-        for (int i = start_row; i < num_row - 1; i++) { // last col
-            A[i][num_row-1] = f(A0[i][num_row-1], A0[i + 1][num_row-1], last_col[i], last_col[i + 1]);
-        }
-    }
-    if (row != n_of_P - 1) { // not the lowerst
-        for (int j = start_col; j < num_col - 1; j++) { // last row
-            A[num_col-1][j] = f(A0[num_col-1][j], last_row[j], A0[num_col-1][j + 1], last_row[j + 1]);
-        }
-    }
-    if (row != n_of_P - 1 && col != n_of_P - 1) { // not the lowest or rightest
-        assert(l_r != -1);
-        A[num_col-1][num_row-1] = f(A0[num_col-1][num_row-1], last_row[num_col-1], last_col[num_row-1], l_r);
-    }
 
-    // handle rightest and lowerest
-    if (col == n_of_P - 1) for (int i = 0; i < num_row; i++) A[i][num_row-1] = A0[i][num_row-1];
-    if (row == n_of_P - 1) for (int j = 0; j < num_col; j++) A[num_col-1][j] = A0[num_col-1][j];
-    if (row == n_of_P - 1 && col == n_of_P - 1) A[num_col-1][num_row-1] = A0[num_col-1][num_row-1];
+        long long last_row[num_col];
+        long long last_col[num_row];
+        long long l_r = -1;
+        // start receiving
+        if (col != n_of_P - 1) { // not the rightest, receive from right
+            MPI_Recv(&last_col, num_row, MPI_LONG_LONG, ID + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        if (row != n_of_P - 1) { // not the lowest
+            MPI_Recv(&last_row, num_col, MPI_LONG_LONG, ID + n_of_P, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        if (row != n_of_P - 1 && col != n_of_P - 1) { // not the lowest or rightest
+            MPI_Recv(&l_r, 1, MPI_LONG_LONG, ID + n_of_P + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
 
-    if (if_print) {
-        cout << "Contents of A of " << ID <<  ": ";
-        for (int i = 0; i < num_row; i++) {
-            for (int j = 0; j < num_col; j++) {
-                cout << A[i][j] << " ";
+        if (if_print && it == 9) {
+            if (col != n_of_P - 1) {
+                cout << "Last col of " << ID << ", ";
+                for (long long elt: last_col) cout << elt << " ";
             }
-            // cout << "\n";
+            if (row != n_of_P - 1) {
+                cout << "Last row of " << ID << ", ";
+                for (long long  elt: last_row) cout << elt << " ";
+            }
+            cout << "LR of " << ID << ", ";
+            cout << l_r << "\n";
         }
-        cout << "\n";
-    }
+
+        // begin calculation.
+        vector<vector<long long>> A(num_row, vector<long long> (num_col, 0));
+        int start_row = 0;
+        int start_col = 0;
+        if (row == 0) {
+            start_row = 1;
+            for (int j = 0; j < num_col; j++) A[0][j] = A0[0][j];
+        }
+        if (col == 0) {
+            start_col = 1;
+            for (int i = 0; i < num_row; i++) A[i][0] = A0[i][0];
+        } // upperest and leftest are handled
+
+        for (int i = start_row; i < num_row - 1; i++) { // handle mid elements
+            for (int j = start_col; j < num_col - 1; j++) {
+                A[i][j] = f(A0[i][j], A0[i + 1][j], A0[i][j + 1], A0[i + 1][j + 1]);
+            }
+        }
+        if (col != n_of_P - 1) { // not the rightest
+            for (int i = start_row; i < num_row - 1; i++) { // last col
+                A[i][num_row-1] = f(A0[i][num_row-1], A0[i + 1][num_row-1], last_col[i], last_col[i + 1]);
+            }
+        }
+        if (row != n_of_P - 1) { // not the lowerst
+            for (int j = start_col; j < num_col - 1; j++) { // last row
+                A[num_col-1][j] = f(A0[num_col-1][j], last_row[j], A0[num_col-1][j + 1], last_row[j + 1]);
+            }
+        }
+        if (row != n_of_P - 1 && col != n_of_P - 1) { // not the lowest or rightest
+            assert(l_r != -1);
+            A[num_col-1][num_row-1] = f(A0[num_col-1][num_row-1], last_row[num_col-1], last_col[num_row-1], l_r);
+        }
+
+        // handle rightest and lowerest
+        if (col == n_of_P - 1) for (int i = 0; i < num_row; i++) A[i][num_row-1] = A0[i][num_row-1];
+        if (row == n_of_P - 1) for (int j = 0; j < num_col; j++) A[num_col-1][j] = A0[num_col-1][j];
+        if (row == n_of_P - 1 && col == n_of_P - 1) A[num_col-1][num_row-1] = A0[num_col-1][num_row-1];
+
+        if (if_print && it == 9) {
+            cout << "Contents of A of " << ID <<  ": ";
+            for (int i = 0; i < num_row; i++) {
+                for (int j = 0; j < num_col; j++) {
+                    cout << A[i][j] << " ";
+                }
+                // cout << "\n";
+            }
+            cout << "\n";
+        }
 
 
-    A0 = A; // important!
+        A0 = A; // important!
 
-    MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Barrier(MPI_COMM_WORLD);
     
-
+    }
 
     return;//sb
 
@@ -215,7 +217,7 @@ int main(int argc, char** argv) {
     
 
     MPI_Barrier(MPI_COMM_WORLD);
-    run_parallel(n, &func, if_print, P, ID);
+    run_parallel(n, &f, if_print, P, ID);
 
     // Finalize MPI.
     MPI_Finalize();
