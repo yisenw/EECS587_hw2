8f4f97b7c09e9397c7d3a2d9d6d85ca1da4545ed
yisenw <yisenw@umich.edu>
2021-10-04 20:35:08 -0400

    587nb

--- submit.sh
+++ submit.sh
@@ -1,1 +1,17 @@
-mpirun -np 36 --oversubscribe pal.out 4000 0
+#!/bin/bash
+# (See https://arc-ts.umich.edu/greatlakes/user-guide/ for command details)
+# Set up batch job settings
+#SBATCH --job-name=mpi4000*4000
+#SBATCH --mail-type=BEGIN, END
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=36
+#SBATCH --mem-per-cpu=1g
+#SBATCH --time=00:05:00
+#SBATCH --account=eecs587f21_class
+#SBATCH --partition=standard
+
+# Run your program
+# (">" redirects the print output of your pfogram,
+# in this case to "output.txt")
+
+mpirun -np 36 --oversubscribe pal.out 4000 0 > out.txt
