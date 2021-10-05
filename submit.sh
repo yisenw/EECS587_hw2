fbce27a1dbd7a2a5e0b1d59435f4dedcb3906c0c
yisenw <yisenw@umich.edu>
2021-10-04 20:56:00 -0400

    587nb

--- submit.sh
+++ submit.sh
@@ -5,9 +5,9 @@
 #SBATCH --job-name=submit
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=36
-#SBATCH --mem-per-cpu=1000m 
-#SBATCH --time=5:00
+#SBATCH --ntasks-per-node=4
+#SBATCH --mem-per-cpu=1g
+#SBATCH --time=00:05:00
 #SBATCH --account=eecs587f21_class
 #SBATCH --partition=standard
 
