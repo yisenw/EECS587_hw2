d0deb15483f605c2d026007bb05abe359349e7d3
yisenw <yisenw@umich.edu>
2021-10-05 00:16:01 -0400

    1000 strange

--- submit_serial_1000.sh
+++ submit_serial_1000.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=1g
-#SBATCH --time=00:10:00
+#SBATCH --time=00:05:00
 #SBATCH --account=eecs587f21_class
 #SBATCH --partition=standard
 
