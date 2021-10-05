8b06e9c234a766c2a19295645514ab59b7860c51
yisenw <yisenw@umich.edu>
2021-10-05 02:34:04 -0400

    fix sum

--- parallel_program.cpp
+++ parallel_program.cpp
@@ -150,17 +150,17 @@ void run_parallel(long long n, long long (*f)(long long, long long, long long, l
                 A[i][j] = f(A0[i][j], A0[i + 1][j], A0[i][j + 1], A0[i + 1][j + 1]);
             }
         }
-        if (col != num_row - 1) { // not the rightest
+        if (col != n_of_P - 1) { // not the rightest
             for (long long i = start_row; i < num_row - 1; i++) { // last col
-                A[i][num_row-1] = f(A0[i][num_col-1], A0[i + 1][num_col-1], last_col[i], last_col[i + 1]);
+                A[i][num_col-1] = f(A0[i][num_col-1], A0[i + 1][num_col-1], last_col[i], last_col[i + 1]);
             }
         }
-        if (row != num_col - 1) { // not the lowerst
+        if (row != n_of_P - 1) { // not the lowerst
             for (long long j = start_col; j < num_col - 1; j++) { // last row
-                A[num_col-1][j] = f(A0[num_row-1][j], last_row[j], A0[num_row-1][j + 1], last_row[j + 1]);
+                A[num_row-1][j] = f(A0[num_row-1][j], last_row[j], A0[num_row-1][j + 1], last_row[j + 1]);
             }
         }
-        if (row != num_col - 1 && col != num_row - 1) { // not the lowest or rightest
+        if (row != n_of_P - 1 && col != n_of_P - 1) { // not the lowest or rightest
             assert(l_r != -1);
             A[num_row-1][num_col-1] = f(A0[num_row-1][num_col-1], last_row[num_col-1], last_col[num_row-1], l_r);
         }
