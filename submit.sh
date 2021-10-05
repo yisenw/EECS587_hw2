71704803f266a5b50e3e46922b0aa7f328d29e69
yisenw <yisenw@umich.edu>
2021-10-04 20:50:15 -0400

    587nb

--- submit.sh
+++ submit.sh
@@ -8,11 +8,11 @@
 #SBATCH --ntasks-per-node=36
 #SBATCH --mem-per-cpu=1000m 
 #SBATCH --time=5:00
-#SBATCH --account=test
+#SBATCH --account=yisenw
 #SBATCH --partition=standard
 
 # Run your program
 # (">" redirects the print output of your pfogram,
 # in this case to "output.txt")
 
-mpirun -np 36 --oversubscribe pal.out 4000 0 > out.txt
+mpirun -np 36 --oversubscribe EECS587_hw2/pal.out 4000 0 > out.txt
